cfdab3f08dd8e58e44f4a79daffd6d9fc8297a6a
Jason Florentino <jasonmflorentino@gmail.com>
2021-10-09 12:35:58 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,47 @@
 module.exports = {
   "env": {
-    "node":     true,
+    "node"    : true,
     "commonjs": true,
-    "es2021":   true
+    "es2021"  : true,
   },
-  "extends":       "eslint:recommended",
+  "extends"      : "eslint:recommended",
   "parserOptions": {
-    "ecmaVersion": 12
+    "ecmaVersion": 12,
   },
   "rules": {
     "array-bracket-newline": ["error", { "minItems": 5 }],
     "array-element-newline": ["error", { "minItems": 5 }],
-    "capitalized-comments":  ["error", "always", { "ignoreConsecutiveComments": true }],
-    "indent":                ["error", 2],
-    "key-spacing":           ["error", {
+    "capitalized-comments" : ["error", "always", { "ignoreConsecutiveComments": true }],
+    "comma-dangle"         : ["error", {
+      "arrays"   : "always-multiline",
+      "objects"  : "always-multiline",
+      "imports"  : "always-multiline",
+      "exports"  : "always-multiline",
+      "functions": "never",
+    }],
+    "indent"     : ["error", 2],
+    "key-spacing": ["error", {
+      "singleLine": {
+        "beforeColon": false,
+        "afterColon" : true,
+      },
       "multiLine": {
         "beforeColon": false,
-        "afterColon":  true
+        "afterColon" : true,
+        "align"      : "colon",
       },
-      "align": {
-        "beforeColon": false,
-        "afterColon":  true,
-        "on":          "value"
-      }
     }],
-    "linebreak-style":      ["error", "unix"],
-    "no-multi-assign":      ["error"],
-    "no-unused-vars":       ["warn"],
+    "linebreak-style"     : ["error", "unix"],
+    "no-multi-assign"     : ["error"],
+    "no-unused-vars"      : ["warn"],
     "object-curly-newline": ["error", {
       "minProperties": 2,
-      "consistent":    true 
+      "consistent"   : true, 
     }],
-    "object-curly-spacing":    ["error", "always", { "arraysInObjects": false }],
+    "object-curly-spacing"   : ["error", "always", { "arraysInObjects": false }],
     "object-property-newline": ["error"],
-    "quotes":                  ["error", "double"],
-    "semi":                    ["error", "always"]
-  }
+    "quotes"                 : ["error", "double"],
+    "semi"                   : ["error", "always"],
+  },
 };
   
