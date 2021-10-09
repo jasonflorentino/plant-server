22fb3341f425a091ed39be3c518c686bb9104c2a
Jason Florentino <jasonmflorentino@gmail.com>
2021-10-09 15:13:40 -0400

    Update formatting

--- utils/rate_limiter.js
+++ utils/rate_limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const ratelimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max:      200 // Max 200 requests per windowMs
+  max     : 200, // Max 200 requests per windowMs
 });
 
 module.exports = ratelimiter;
